d183ad0f94ab21027bb0e103b2a420bc43db0f0c
Yi Yao <leaf_yao8621@hotmail.com>
2022-08-01 15:00:49 -0700

    Hashmap FWP

--- v2/src/parser/parser.c
+++ v2/src/parser/parser.c
@@ -3,6 +3,8 @@
 DEF_DRRAY_FUNCTIONS(Token)
 DEF_DRRAY_FUNCTIONS(Character)
 DEF_HASHSET_FUNCTIONS(String)
+DEF_HASHMAP_FUNCTIONS(String, String)
+DEF_HASHMAP_FUNCTIONS(String, Size)
 
 int parser_initialize(Parser *parser, String ifn) {
     if (!parser || !ifn) {
@@ -39,6 +41,19 @@ int parser_initialize(Parser *parser, String ifn) {
         DArrayCharacter_finalize(&parser->str_buf);
         return ret;
     }
+    ret =
+        HashMapStringString_initialize(
+            &parser->str_name,
+            1, hash_function_string,
+            eq_function_string
+        );
+    if (ret) {
+        DArrayToken_finalize(&parser->tokens);
+        DArrayToken_finalize(&parser->stack);
+        DArrayCharacter_finalize(&parser->str_buf);
+        HashSetString_finalize(&parser->int_name);
+        return ret;
+    }
     return 0;
 }
 
@@ -59,6 +74,10 @@ int parser_finalize(Parser *parser) {
         return ret;
     }
     ret = HashSetString_finalize(&parser->int_name);
+    if (ret) {
+        return ret;
+    }
+    ret = HashMapStringString_finalize(&parser->str_name);
     if (ret) {
         return ret;
     }
@@ -99,26 +118,47 @@ int parser_parse(Parser *parser) {
     DArrayCharacter_push(&parser->str_buf, "d");
     DArrayCharacter_push(&parser->str_buf, "");
     char *str = parser->str_buf.data;
+    char **ptr = NULL;
     HashSetString_insert(&parser->int_name, &str);
+    HashMapStringString_fetch(&parser->str_name, &str, &ptr);
+    *ptr = "alpha";
     str += 5;
     HashSetString_insert(&parser->int_name, &str);
+    HashMapStringString_fetch(&parser->str_name, &str, &ptr);
+    *ptr = "bravo";
     str += 4;
     HashSetString_insert(&parser->int_name, &str);
+    HashMapStringString_fetch(&parser->str_name, &str, &ptr);
+    *ptr = "charlie";
     str += 5;
     HashSetString_insert(&parser->int_name, &str);
+    HashMapStringString_fetch(&parser->str_name, &str, &ptr);
+    *ptr = "delta";
     str += 5;
     HashSetString_insert(&parser->int_name, &str);
+    HashMapStringString_fetch(&parser->str_name, &str, &ptr);
+    *ptr = "echo";
     str += 5;
     HashSetString_insert(&parser->int_name, &str);
+    HashMapStringString_fetch(&parser->str_name, &str, &ptr);
+    *ptr = "foxtrot";
     for (size_t i = 0; i < parser->int_name.capacity; ++i) {
         printf("in_use: %lu\nitem: %s\n", parser->int_name.data[i].in_use, parser->int_name.data[i].item);
+    }
+    for (size_t i = 0; i < parser->str_name.capacity; ++i) {
+        printf("in_use: %lu\nkey: %s\nvalue: %s\n", parser->str_name.data[i].in_use, parser->str_name.data[i].key, parser->str_name.data[i].value);
     }
     bool out = false;
     str = "cdef";
     HashSetString_check(&parser->int_name, &str, &out);
     printf("cdef: %d\n", out);
+    HashMapStringString_check(&parser->str_name, &str, &out);
+    HashMapStringString_fetch(&parser->str_name, &str, &ptr);
+    printf("cdef: %d %s\n", out, *ptr);
     str = "abcde";
     HashSetString_check(&parser->int_name, &str, &out);
     printf("abcde: %d\n", out);
+    HashMapStringString_check(&parser->str_name, &str, &out);
+    printf("cdef: %d\n", out);
     return 0;
 }
--- v2/src/util/hash_map.h
+++ v2/src/util/hash_map.h
@@ -16,4 +16,153 @@ typedef struct HashMap##Key##Value {\
     bool (*eq)(Key*, Key*);\
 } HashMap##Key##Value;
 
+#define DEF_HASHMAP_FUNCTIONS(Key, Value)\
+int HashMap##Key##Value##_initialize(\
+    HashMap##Key##Value *hashset,\
+    size_t capacity,\
+    size_t (*hash)(Key*),\
+    bool (*eq)(Key*, Key*)\
+);\
+int HashMap##Key##Value##_fetch(\
+    HashMap##Key##Value *hashmap,\
+    Key *key,\
+    Value **value\
+);\
+int HashMap##Key##Value##_check(\
+    HashMap##Key##Value *hashmap,\
+    Key *key,\
+    bool *out\
+);\
+int HashMap##Key##Value##_finalize(HashMap##Key##Value *hashmap);\
+int HashMap##Key##Value##_initialize(\
+    HashMap##Key##Value *hashmap,\
+    size_t capacity,\
+    size_t (*hash)(Key*),\
+    bool (*eq)(Key*, Key*)\
+) {\
+    if (!hashmap || !hash) {\
+        return ERR_NULL_PTR;\
+    }\
+    hashmap->capacity = capacity;\
+    hashmap->hash = hash;\
+    hashmap->eq = eq;\
+    hashmap->data = calloc(capacity, sizeof(HashMap##Key##Value##Node));\
+    if (!hashmap->data) {\
+        return ERR_OUT_OF_MEMORY;\
+    }\
+    return 0;\
+}\
+int HashMap##Key##Value##_fetch(\
+    HashMap##Key##Value *hashmap,\
+    Key *key,\
+    Value **value\
+) {\
+    if (!hashmap || !key) {\
+        return ERR_NULL_PTR;\
+    }\
+    size_t idx = hashmap->hash(key) % hashmap->capacity;\
+    HashMap##Key##Value##Node *iter = hashmap->data + idx;\
+    for (\
+        ;\
+        idx < hashmap->capacity &&\
+        iter->in_use &&\
+        !hashmap->eq(&iter->key, key);\
+        ++idx,\
+        ++iter\
+    );\
+    if (idx < hashmap->capacity && iter->in_use) {\
+        *value = &iter->value;\
+        return 0;\
+    }\
+    if (idx == hashmap->capacity) {\
+        HashMap##Key##Value##Node *buf = 0;\
+        size_t new_capacity = hashmap->capacity << 1;\
+        bool cont = false;\
+        do {\
+            buf = calloc(new_capacity, sizeof(HashMap##Key##Value##Node));\
+            if (!buf) {\
+                return ERR_OUT_OF_MEMORY;\
+            }\
+            cont = false;\
+            HashMap##Key##Value##Node *iter_old = hashmap->data;\
+            for (size_t i = 0; i < hashmap->capacity; ++i, ++iter_old) {\
+                if (!iter_old->in_use) {\
+                    continue;\
+                }\
+                size_t idx_new = hashmap->hash(&iter_old->key) % new_capacity;\
+                HashMap##Key##Value##Node *iter_new = buf + idx_new;\
+                for (\
+                    ;\
+                    idx < new_capacity &&\
+                    iter_new->in_use;\
+                    ++idx_new,\
+                    ++iter_new\
+                );\
+                if (idx == new_capacity) {\
+                    cont = true;\
+                    break;\
+                }\
+                iter_new->key = iter_old->key;\
+                iter_new->value = iter_old->value;\
+                iter_new->in_use = true;\
+            }\
+            idx = hashmap->hash(key) % hashmap->capacity;\
+            iter = buf + idx;\
+            for (\
+                ;\
+                idx < new_capacity &&\
+                iter->in_use;\
+                ++idx,\
+                ++iter\
+            );\
+            if (idx == new_capacity) {\
+                cont = true;\
+            }\
+            if (cont) {\
+                new_capacity <<= 1;\
+                free(buf);\
+            }\
+        } while (cont);\
+        free(hashmap->data);\
+        hashmap->data = buf;\
+        hashmap->capacity = new_capacity;\
+    }\
+    iter->key = *key;\
+    iter->in_use = true;\
+    *value = &iter->value;\
+    return 0;\
+}\
+int HashMap##Key##Value##_check(\
+    HashMap##Key##Value *hashmap,\
+    Key *key,\
+    bool *out\
+) {\
+    if (!hashmap || !key || !out) {\
+        return ERR_NULL_PTR;\
+    }\
+    size_t idx = hashmap->hash(key) % hashmap->capacity;\
+    HashMap##Key##Value##Node *iter = hashmap->data + idx;\
+    for (\
+        ;\
+        idx < hashmap->capacity &&\
+        iter->in_use &&\
+        !hashmap->eq(&iter->key, key);\
+        ++idx,\
+        ++iter\
+    );\
+    if (idx == hashmap->capacity || !iter->in_use) {\
+        *out = false;\
+    } else {\
+        *out = true;\
+    }\
+    return 0;\
+}\
+int HashMap##Key##Value##_finalize(HashMap##Key##Value *hashmap) {\
+    if (!hashmap) {\
+        return ERR_NULL_PTR;\
+    }\
+    free(hashmap->data);\
+    return 0;\
+}
+
 #endif
