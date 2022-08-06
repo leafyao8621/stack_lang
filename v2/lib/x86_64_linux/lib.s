5ec4d2d5c5c8547d3262a6c5442db309c9562ebd
Yi Yao <leaf_yao8621@hotmail.com>
2022-08-06 00:04:16 -0700

    Started library

--- v2/lib/x86_64_linux/lib.s
+++ v2/lib/x86_64_linux/lib.s
@@ -1,10 +1,56 @@
-print:
-    movq $1, %rdi
+print_str:
+    movabsq $1, %rdi
     movq stack_ptr, %rax
     subq $8, %rax
     movq %rax, stack_ptr
     movq (%rax), %rsi
-    movq $4, %rdx
-    movq $1, %rax
+    movq strlen, %rdx
+    movabsq $1, %rax
+    syscall
+    ret
+print_chr:
+    movabsq $1, %rdi
+    movq stack_ptr, %rax
+    subq $8, %rax
+    movq %rax, stack_ptr
+    movb (%rax), %bl
+    movb %bl, buf
+    movabsq $buf, %rsi
+    movabsq $1, %rdx
+    movabsq $1, %rax
+    syscall
+    ret
+println_str:
+    movabsq $1, %rdi
+    movq stack_ptr, %rax
+    subq $8, %rax
+    movq %rax, stack_ptr
+    movq (%rax), %rsi
+    movq strlen, %rdx
+    movabsq $1, %rax
+    syscall
+    movabsq $1, %rdi
+    movabsq $buf, %rsi
+    movb $10, buf
+    movabsq $1, %rdx
+    movabsq $1, %rax
+    syscall
+    ret
+println_chr:
+    movabsq $1, %rdi
+    movq stack_ptr, %rax
+    subq $8, %rax
+    movq %rax, stack_ptr
+    movb (%rax), %bl
+    movb %bl, buf
+    movabsq $buf, %rsi
+    movabsq $1, %rdx
+    movabsq $1, %rax
+    syscall
+    movabsq $1, %rdi
+    movabsq $buf, %rsi
+    movb $10, buf
+    movabsq $1, %rdx
+    movabsq $1, %rax
     syscall
     ret
--- v2/lib/x86_64_linux/main.s
+++ v2/lib/x86_64_linux/main.s
@@ -1,19 +1,62 @@
     .section .data
 buf:
-    .fill 1000
+    .fill 100
+int_buf:
+    .fill 100
 stack:
-    .fill 1000
+    .fill 800
 stack_ptr:
     .quad 0
-    .section .text
-    .globl main
-    .include "lib.s"
+strlen:
+    .quad 0
 str0:
-    .asciz "abc\n"
-main:
-    movabs $stack, %rax
-    movabs %rax, stack_ptr
-    movq $str0, (%rax)
-    addq $8, (%rax)
+    .asciz "abc"
+    .section .text
+    .globl _start
+    .include "lib.s"
+_start:
+    movabsq $stack, %rax
     movq %rax, stack_ptr
-    call print
+
+    movq stack_ptr, %rax
+    movq $str0, (%rax)
+    movabsq $3, %rbx
+    movq %rbx, strlen
+    addq $8, %rax
+    movq %rax, stack_ptr
+    call print_str
+    movq stack_ptr, %rax
+    movabsq $10, %rbx
+    movq %rbx, (%rax)
+    addq $8, %rax
+    movq %rax, stack_ptr
+    call print_chr
+    movq stack_ptr, %rax
+    movabsq $65, %rbx
+    movq %rbx, (%rax)
+    addq $8, %rax
+    movq %rax, stack_ptr
+    call print_chr
+    movq stack_ptr, %rax
+    movabsq $10, %rbx
+    movq %rbx, (%rax)
+    addq $8, %rax
+    movq %rax, stack_ptr
+    call print_chr
+    movq stack_ptr, %rax
+    movq $str0, (%rax)
+    movabsq $3, %rbx
+    movq %rbx, strlen
+    addq $8, %rax
+    movq %rax, stack_ptr
+    call println_str
+    movq stack_ptr, %rax
+    movabsq $97, %rbx
+    movq %rbx, (%rax)
+    addq $8, %rax
+    movq %rax, stack_ptr
+    call println_chr
+    movq $60, %rax
+    movq $0, %rdi
+    syscall
+
