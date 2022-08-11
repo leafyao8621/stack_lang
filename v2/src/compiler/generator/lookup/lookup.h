c71a1565d7c11bcf91c555e25811c584b7bc10a4
Yi Yao <leaf_yao8621@hotmail.com>
2022-08-11 13:47:55 -0700

    Started generator

---
+++ v2/src/compiler/generator/lookup/lookup.h
@@ -0,0 +1,6 @@
+#ifndef LOOKUP_H_
+#define LOOKUP_H_
+
+#include "x86_64_linux/lookup_x86_64_linux.h"
+
+#endif
---
+++ v2/src/compiler/generator/lookup/x86_64_linux/lookup_x86_64_linux.h
@@ -0,0 +1,6 @@
+#ifndef LOOKUP_X86_64_LINUX_H_
+#define LOOKUP_X86_64_LINUX_H_
+
+extern const char *data_section_x86_64_linux;
+
+#endif
