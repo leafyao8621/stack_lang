c38f93f4219f4ce3c521b26ce55cea31eb7abb0b
Yi Yao <leaf_yao8621@hotmail.com>
2022-08-11 15:21:27 -0700

    Updates

--- v2/lib/x86_64_linux/main.s
+++ v2/lib/x86_64_linux/main.s
@@ -33,34 +33,34 @@ _start:
     movq %rax, stack_ptr
     call print_chr
 
-    // movq stack_ptr, %rax
-    // movabsq $65, %rbx
-    // movq %rbx, (%rax)
-    // addq $8, %rax
-    // movq %rax, stack_ptr
-    // call print_chr
+    movq stack_ptr, %rax
+    movabsq $65, %rbx
+    movq %rbx, (%rax)
+    addq $8, %rax
+    movq %rax, stack_ptr
+    call print_chr
 
-    // movq stack_ptr, %rax
-    // movabsq $10, %rbx
-    // movq %rbx, (%rax)
-    // addq $8, %rax
-    // movq %rax, stack_ptr
-    // call print_chr
+    movq stack_ptr, %rax
+    movabsq $10, %rbx
+    movq %rbx, (%rax)
+    addq $8, %rax
+    movq %rax, stack_ptr
+    call print_chr
 
-    // movq stack_ptr, %rax
-    // movq $str0, (%rax)
-    // movabsq $3, %rbx
-    // movq %rbx, strlen
-    // addq $8, %rax
-    // movq %rax, stack_ptr
-    // call println_str
+    movq stack_ptr, %rax
+    movq $str0, (%rax)
+    movabsq $3, %rbx
+    movq %rbx, strlen
+    addq $8, %rax
+    movq %rax, stack_ptr
+    call println_str
 
-    // movq stack_ptr, %rax
-    // movabsq $97, %rbx
-    // movq %rbx, (%rax)
-    // addq $8, %rax
-    // movq %rax, stack_ptr
-    // call println_chr
+    movq stack_ptr, %rax
+    movabsq $97, %rbx
+    movq %rbx, (%rax)
+    addq $8, %rax
+    movq %rax, stack_ptr
+    call println_chr
     
     movq stack_ptr, %rax
     movabsq $-123456, %rbx
