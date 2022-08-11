82134eb1684329b5a544967bd9d194a9d18921e7
Yi Yao <leaf_yao8621@hotmail.com>
2022-08-11 14:50:03 -0700

    print int non-negative

--- v2/lib/x86_64_linux/lib.s
+++ v2/lib/x86_64_linux/lib.s
@@ -54,3 +54,38 @@ println_chr:
     movabsq $1, %rax
     syscall
     ret
+print_int:
+    movabsq $1, %rdi
+    movq stack_ptr, %rax
+    subq $8, %rax
+    movq %rax, stack_ptr
+    movq (%rax), %rax
+    movabsq $int_buf, %r10
+    movabsq $0, %r11
+    movabsq $0, %r12
+    movabsq $10, %rbx
+print_int_loop0:
+    divq %rbx
+    addq $48, %rdx
+    movb %dl, (%r10)
+    movabsq $0, %rdx
+    addq $1, %r10
+    addq $1, %r11
+    cmpq %rax, %r12
+    jne print_int_loop0
+    movq %r11, strlen
+    movabsq $buf, %r12
+    movabsq $0, %r14
+print_int_loop1:
+    subq $1, %r10
+    movb (%r10), %r13b
+    movb %r13b, (%r12)
+    addq $1, %r12
+    subq $1, %r11
+    cmpq %r11, %r14
+    jne print_int_loop1
+    movabsq $buf, %rsi
+    movq strlen, %rdx
+    movabsq $1, %rax
+    syscall
+    ret
--- v2/lib/x86_64_linux/main.s
+++ v2/lib/x86_64_linux/main.s
@@ -18,48 +18,55 @@ _start:
     movabsq $stack, %rax
     movq %rax, stack_ptr
 
-    movq stack_ptr, %rax
-    movq $str0, (%rax)
-    movabsq $3, %rbx
-    movq %rbx, strlen
-    addq $8, %rax
-    movq %rax, stack_ptr
-    call print_str
+    // movq stack_ptr, %rax
+    // movq $str0, (%rax)
+    // movabsq $3, %rbx
+    // movq %rbx, strlen
+    // addq $8, %rax
+    // movq %rax, stack_ptr
+    // call print_str
 
-    movq stack_ptr, %rax
-    movabsq $10, %rbx
-    movq %rbx, (%rax)
-    addq $8, %rax
-    movq %rax, stack_ptr
-    call print_chr
-    movq stack_ptr, %rax
-    movabsq $65, %rbx
-    movq %rbx, (%rax)
-    addq $8, %rax
-    movq %rax, stack_ptr
-    call print_chr
+    // movq stack_ptr, %rax
+    // movabsq $10, %rbx
+    // movq %rbx, (%rax)
+    // addq $8, %rax
+    // movq %rax, stack_ptr
+    // call print_chr
+    // movq stack_ptr, %rax
+    // movabsq $65, %rbx
+    // movq %rbx, (%rax)
+    // addq $8, %rax
+    // movq %rax, stack_ptr
+    // call print_chr
 
-    movq stack_ptr, %rax
-    movabsq $10, %rbx
-    movq %rbx, (%rax)
-    addq $8, %rax
-    movq %rax, stack_ptr
-    call print_chr
+    // movq stack_ptr, %rax
+    // movabsq $10, %rbx
+    // movq %rbx, (%rax)
+    // addq $8, %rax
+    // movq %rax, stack_ptr
+    // call print_chr
 
-    movq stack_ptr, %rax
-    movq $str0, (%rax)
-    movabsq $3, %rbx
-    movq %rbx, strlen
-    addq $8, %rax
-    movq %rax, stack_ptr
-    call println_str
-    movq stack_ptr, %rax
-    movabsq $97, %rbx
-    movq %rbx, (%rax)
-    addq $8, %rax
-    movq %rax, stack_ptr
-    call println_chr
+    // movq stack_ptr, %rax
+    // movq $str0, (%rax)
+    // movabsq $3, %rbx
+    // movq %rbx, strlen
+    // addq $8, %rax
+    // movq %rax, stack_ptr
+    // call println_str
+    // movq stack_ptr, %rax
+    // movabsq $97, %rbx
+    // movq %rbx, (%rax)
+    // addq $8, %rax
+    // movq %rax, stack_ptr
+    // call println_chr
     
+    movq stack_ptr, %rax
+    movabsq $123, %rbx
+    movq %rbx, (%rax)
+    addq $8, %rax
+    movq %rax, stack_ptr
+    call print_int
+
     movq $60, %rax
     movq $0, %rdi
     syscall
