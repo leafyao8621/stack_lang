414c5637d8b11a680ccb20ed3ba9826c5d466e59
Yi Yao <leaf_yao8621@hotmail.com>
2022-08-22 14:52:26 -0700

    Updated arm lib

--- v2/lib/arm64_linux/lib.s
+++ v2/lib/arm64_linux/lib.s
@@ -1,3 +1,20 @@
+calc_strlen:
+    ldr x9, =stack_ptr
+    ldr x10, [x9]
+    sub x10, x10, #8
+    ldr x11, [x10]
+    mov x12, #0
+calc_strlen_loop0:
+    ldrb w13, [x11]
+    cmp x13, #0
+    beq calc_strlen_end_loop0
+    add x11, x11, 1
+    add x12, x12, 1
+    b calc_strlen_loop0
+calc_strlen_end_loop0:
+    ldr x9, =strlen
+    str x12, [x9]
+    ret
 print_str:
     ldr x9, =stack_ptr
     ldr x10, [x9]
@@ -10,5 +27,27 @@ print_str:
     ldr x9, =strlen
     ldr x10, [x9]
     mov x2, x10
+    svc #0
+    ret
+println_str:
+    ldr x9, =stack_ptr
+    ldr x10, [x9]
+    sub x10, x10, #8
+    str x10, [x9]
+    ldr x11, [x10]
+    mov x8, #64
+    mov x0, #1
+    mov x1, x11
+    ldr x9, =strlen
+    ldr x10, [x9]
+    mov x2, x10
+    svc #0
+    ldr x9, =str_buf
+    mov x10, #10
+    strb w10, [x9]
+    mov x8, #64
+    mov x0, #1
+    mov x1, x9
+    mov x2, #1
     svc #0
     ret
--- v2/lib/arm64_linux/main.s
+++ v2/lib/arm64_linux/main.s
@@ -2,6 +2,8 @@
 stack: .fill 100, 8, 0
 stack_ptr: .quad 0
 strlen: .quad 0
+str_buf: .fill 100
+int_buf: .fill 100
 str0: .asciz "abc"
     .text
     .global _start
@@ -17,12 +19,17 @@ _start:
     str x11, [x10]
     add x10, x10, #8
     str x10, [x9]
-
-    ldr x9, =strlen
-    mov x10, #3
-    str x10, [x9]
-
+    bl calc_strlen
     bl print_str
+
+    ldr x9, =stack_ptr
+    ldr x10, [x9]
+    ldr x11, =str0
+    str x11, [x10]
+    add x10, x10, #8
+    str x10, [x9]
+    bl calc_strlen
+    bl println_str
 
     mov x8, #93
     mov x0, #0
