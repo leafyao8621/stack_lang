6d97cbf62d29a50c24589bd1d4bad4548ca1cb25
Yi Yao <leaf_yao8621@hotmail.com>
2022-08-25 15:27:47 -0700

    bnot

--- programs/v2/test.sl
+++ programs/v2/test.sl
@@ -1,7 +1,10 @@
-#a -8 =
+#a -1 =
 #b 2 =
 
-8 2 << _println
-#a 2 << _println
-8 #b << _println
-#a #b << _println
+; 8 2 << _println
+; #a 2 << _println
+; 8 #b << _println
+; #a #b << _println
+
+-1 ~ _println
+#a ~ _println
--- v2/src/compiler/generator/handler/arm64_linux/handler_arm64_linux.c
+++ v2/src/compiler/generator/handler/arm64_linux/handler_arm64_linux.c
@@ -1754,26 +1754,30 @@ static int handle_token_operator(
         switch (op1.type) {
         case TOKEN_INT_LIT:
             fputs(
-                "    movq stack_ptr, %rax\n"
-                "    subq $8, %rax\n"
-                "    movq (%rax), %rbx\n"
-                "    notq %rbx\n"
-                "    movq %rbx, (%rax)\n"
-                "    addq $8, %rax\n"
-                "    movq %rax, stack_ptr\n",
+                "    ldr x9, =stack_ptr\n"
+                "    ldr x10, [x9]\n"
+                "    sub x10, x10, #8\n"
+                "    ldr x11, [x10]\n"
+                "    mov x12, #-1\n"
+                "    eor x11, x11, x12\n"
+                "    str x11, [x10]\n"
+                "    add x10, x10, 8\n"
+                "    str x10, [x9]\n",
                 fasm
             );
             break;
         case TOKEN_INT_NAME:
             fputs(
-                "    movq stack_ptr, %rax\n"
-                "    subq $8, %rax\n"
-                "    movq (%rax), %rbx\n"
-                "    movq (%rbx), %rbx\n"
-                "    notq %rbx\n"
-                "    movq %rbx, (%rax)\n"
-                "    addq $8, %rax\n"
-                "    movq %rax, stack_ptr\n",
+                "    ldr x9, =stack_ptr\n"
+                "    ldr x10, [x9]\n"
+                "    sub x10, x10, #8\n"
+                "    ldr x11, [x10]\n"
+                "    ldr x11, [x11]\n"
+                "    mov x12, #-1\n"
+                "    eor x11, x11, x12\n"
+                "    str x11, [x10]\n"
+                "    add x10, x10, 8\n"
+                "    str x10, [x9]\n",
                 fasm
             );
             break;
