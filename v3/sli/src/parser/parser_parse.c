579fc714c1f6ea219420aa2497ec79133a79447f
Yi Yao <leaf_yao8621@hotmail.com>
2023-05-04 13:55:08 -0700

    Finished -, +

--- v3/sli/src/parser/parser_parse.c
+++ v3/sli/src/parser/parser_parse.c
@@ -845,10 +845,194 @@ SLErrCode handle_operator(
                 return SL_ERR_TYPE_MISMATCH;
             }
             break;
+        case SL_OPERATOR_TYPE_SUBTRACT_ASSIGN:
+            ret = DArraySLToken_pop_back(&buffer->operation_stack);
+            if (ret) {
+                return SL_ERR_MISSING_OPERAND;
+            }
+            ret = DArraySLToken_pop_back(&buffer->operation_stack);
+            if (ret) {
+                return SL_ERR_MISSING_OPERAND;
+            }
+            switch (
+                buffer
+                    ->operation_stack
+                    .data[buffer->operation_stack.size]
+                    .type) {
+            case SL_TOKEN_TYPE_INT_VAR:
+            case SL_TOKEN_TYPE_FLOAT_VAR:
+                switch (
+                    buffer
+                        ->operation_stack
+                        .data[buffer->operation_stack.size + 1]
+                        .type) {
+                case SL_TOKEN_TYPE_INT_LITERAL:
+                case SL_TOKEN_TYPE_INT_VAR:
+                case SL_TOKEN_TYPE_FLOAT_LITERAL:
+                case SL_TOKEN_TYPE_FLOAT_VAR:
+                    break;
+                default:
+                    return SL_ERR_TYPE_MISMATCH;
+                }
+                break;
+            default:
+                return SL_ERR_TYPE_MISMATCH;
+            }
         default:
             break;
         }
-
+        break;
+    case '+':
+        switch ((*iter)[1]) {
+        case ' ':
+        case '\t':
+        case '\n':
+        case '\0':
+            token.data.operator = SL_OPERATOR_TYPE_ADD;
+            break;
+        case '+':
+            token.data.operator = SL_OPERATOR_TYPE_INC;
+            ++(*iter);
+            break;
+        case '=':
+            token.data.operator = SL_OPERATOR_TYPE_ADD_ASSIGN;
+            ++(*iter);
+            break;
+        default:
+            return SL_ERR_INVALID_OPERATOR;
+        }
+        switch (token.data.operator) {
+        case SL_OPERATOR_TYPE_ADD:
+            ret = DArraySLToken_pop_back(&buffer->operation_stack);
+            if (ret) {
+                return SL_ERR_MISSING_OPERAND;
+            }
+            ret = DArraySLToken_pop_back(&buffer->operation_stack);
+            if (ret) {
+                return SL_ERR_MISSING_OPERAND;
+            }
+            switch (
+                buffer
+                    ->operation_stack
+                    .data[buffer->operation_stack.size]
+                    .type) {
+            case SL_TOKEN_TYPE_INT_LITERAL:
+            case SL_TOKEN_TYPE_INT_VAR:
+                switch (
+                    buffer
+                        ->operation_stack
+                        .data[buffer->operation_stack.size + 1]
+                        .type) {
+                case SL_TOKEN_TYPE_INT_LITERAL:
+                case SL_TOKEN_TYPE_INT_VAR:
+                    token_res.type = SL_TOKEN_TYPE_INT_LITERAL;
+                    ret =
+                        DArraySLToken_push_back(
+                            &buffer->operation_stack,
+                            &token_res
+                        );
+                    if (ret) {
+                        return SL_ERR_OUT_OF_MEMORY;
+                    }
+                    break;
+                case SL_TOKEN_TYPE_FLOAT_LITERAL:
+                case SL_TOKEN_TYPE_FLOAT_VAR:
+                    token_res.type = SL_TOKEN_TYPE_FLOAT_LITERAL;
+                    ret =
+                        DArraySLToken_push_back(
+                            &buffer->operation_stack,
+                            &token_res
+                        );
+                    if (ret) {
+                        return SL_ERR_OUT_OF_MEMORY;
+                    }
+                    break;
+                default:
+                    return SL_ERR_TYPE_MISMATCH;
+                }
+                break;
+            case SL_TOKEN_TYPE_FLOAT_LITERAL:
+            case SL_TOKEN_TYPE_FLOAT_VAR:
+                switch (
+                    buffer
+                        ->operation_stack
+                        .data[buffer->operation_stack.size + 1]
+                        .type) {
+                case SL_TOKEN_TYPE_INT_LITERAL:
+                case SL_TOKEN_TYPE_INT_VAR:
+                case SL_TOKEN_TYPE_FLOAT_LITERAL:
+                case SL_TOKEN_TYPE_FLOAT_VAR:
+                    token_res.type = SL_TOKEN_TYPE_FLOAT_LITERAL;
+                    ret =
+                        DArraySLToken_push_back(
+                            &buffer->operation_stack,
+                            &token_res
+                        );
+                    if (ret) {
+                        return SL_ERR_OUT_OF_MEMORY;
+                    }
+                    break;
+                default:
+                    return SL_ERR_TYPE_MISMATCH;
+                }
+                break;
+            default:
+                return SL_ERR_TYPE_MISMATCH;
+            }
+            break;
+        case SL_OPERATOR_TYPE_INC:
+            ret = DArraySLToken_pop_back(&buffer->operation_stack);
+            if (ret) {
+                return SL_ERR_MISSING_OPERAND;
+            }
+            switch (
+                buffer
+                    ->operation_stack
+                    .data[buffer->operation_stack.size]
+                    .type) {
+            case SL_TOKEN_TYPE_INT_VAR:
+                break;
+            default:
+                return SL_ERR_TYPE_MISMATCH;
+            }
+            break;
+        case SL_OPERATOR_TYPE_ADD_ASSIGN:
+            ret = DArraySLToken_pop_back(&buffer->operation_stack);
+            if (ret) {
+                return SL_ERR_MISSING_OPERAND;
+            }
+            ret = DArraySLToken_pop_back(&buffer->operation_stack);
+            if (ret) {
+                return SL_ERR_MISSING_OPERAND;
+            }
+            switch (
+                buffer
+                    ->operation_stack
+                    .data[buffer->operation_stack.size]
+                    .type) {
+            case SL_TOKEN_TYPE_INT_VAR:
+            case SL_TOKEN_TYPE_FLOAT_VAR:
+                switch (
+                    buffer
+                        ->operation_stack
+                        .data[buffer->operation_stack.size + 1]
+                        .type) {
+                case SL_TOKEN_TYPE_INT_LITERAL:
+                case SL_TOKEN_TYPE_INT_VAR:
+                case SL_TOKEN_TYPE_FLOAT_LITERAL:
+                case SL_TOKEN_TYPE_FLOAT_VAR:
+                    break;
+                default:
+                    return SL_ERR_TYPE_MISMATCH;
+                }
+                break;
+            default:
+                return SL_ERR_TYPE_MISMATCH;
+            }
+        default:
+            break;
+        }
+        break;
     }
     ret = DArraySLToken_push_back(buffer->cur_token_buf, &token);
     if (ret) {
