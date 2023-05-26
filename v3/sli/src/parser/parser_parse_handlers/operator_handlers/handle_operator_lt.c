bd9312dcba2c086012e172d520799733a198ce6c
Yi Yao <leaf_yao8621@hotmail.com>
2023-05-26 10:57:02 -0700

    lt leq

--- v3/sli/src/parser/parser_parse_handlers/operator_handlers/handle_operator_lt.c
+++ v3/sli/src/parser/parser_parse_handlers/operator_handlers/handle_operator_lt.c
@@ -12,6 +12,16 @@ SLErrCode handle_operator_lt(
     int ret = 0;
     SLToken token_res;
     switch ((*iter)[1]) {
+    case ' ':
+    case '\t':
+    case '\n':
+    case '\0':
+        token->data.operator_type = SL_OPERATOR_TYPE_LT;
+        break;
+    case '=':
+        token->data.operator_type = SL_OPERATOR_TYPE_LEQ;
+        ++(*iter);
+        break;
     case '<':
         switch ((*iter)[2]) {
         case ' ':
@@ -57,6 +67,71 @@ SLErrCode handle_operator_lt(
             case SL_TOKEN_TYPE_INT_LITERAL:
             case SL_TOKEN_TYPE_INT_VAR:
                 token_res.type = SL_TOKEN_TYPE_INT_LITERAL;
+                ret =
+                    DArraySLToken_push_back(
+                        &buffer->operation_stack,
+                        &token_res
+                    );
+                if (ret) {
+                    return SL_ERR_OUT_OF_MEMORY;
+                }
+                break;
+            default:
+                return SL_ERR_TYPE_MISMATCH;
+            }
+            break;
+        case SL_TOKEN_TYPE_CHAR_LITERAL:
+        case SL_TOKEN_TYPE_CHAR_VAR:
+            switch (
+                buffer
+                    ->operation_stack
+                    .data[buffer->operation_stack.size + 1]
+                    .type) {
+            case SL_TOKEN_TYPE_CHAR_LITERAL:
+            case SL_TOKEN_TYPE_CHAR_VAR:
+                token_res.type = SL_TOKEN_TYPE_CHAR_LITERAL;
+                ret =
+                    DArraySLToken_push_back(
+                        &buffer->operation_stack,
+                        &token_res
+                    );
+                if (ret) {
+                    return SL_ERR_OUT_OF_MEMORY;
+                }
+                break;
+            default:
+                return SL_ERR_TYPE_MISMATCH;
+            }
+            break;
+        default:
+            return SL_ERR_TYPE_MISMATCH;
+        }
+        break;
+    case SL_OPERATOR_TYPE_LT:
+    case SL_OPERATOR_TYPE_LEQ:
+        ret = DArraySLToken_pop_back(&buffer->operation_stack);
+        if (ret) {
+            return SL_ERR_MISSING_OPERAND;
+        }
+        ret = DArraySLToken_pop_back(&buffer->operation_stack);
+        if (ret) {
+            return SL_ERR_MISSING_OPERAND;
+        }
+        switch (
+            buffer
+                ->operation_stack
+                .data[buffer->operation_stack.size]
+                .type) {
+        case SL_TOKEN_TYPE_INT_LITERAL:
+        case SL_TOKEN_TYPE_INT_VAR:
+            switch (
+                buffer
+                    ->operation_stack
+                    .data[buffer->operation_stack.size + 1]
+                    .type) {
+            case SL_TOKEN_TYPE_INT_LITERAL:
+            case SL_TOKEN_TYPE_INT_VAR:
+                token_res.type = SL_TOKEN_TYPE_CHAR_LITERAL;
                 ret =
                     DArraySLToken_push_back(
                         &buffer->operation_stack,
