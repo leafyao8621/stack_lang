ae5830c5a9c92a07fa723c61667dc800168b94e0
Yi Yao <leaf_yao8621@hotmail.com>
2023-05-31 12:02:03 -0700

    Started interpreter

--- v3/sli/src/interpreter/core/core.h
+++ v3/sli/src/interpreter/core/core.h
@@ -5,6 +5,13 @@
 
 typedef struct SLInterpreter {
     SLParser parser;
+    String global;
+    SLToken *current;
 } SLInterpreter;
+
+SLErrCode SLInterpreter_initialize(SLInterpreter *interpreter);
+SLErrCode SLInterpreter_parse(SLInterpreter *interpreter, char *str);
+SLErrCode SLInterpreter_run(SLInterpreter *interpreter);
+SLErrCode SLInterpreter_finalize(SLInterpreter *intepreter)
 
 #endif
