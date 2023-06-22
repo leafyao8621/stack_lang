9c3d1367a573f63147bc8cf3174a80521436eaab
Yi Yao <leaf_yao8621@hotmail.com>
2023-06-22 13:51:57 -0700

    Added error code

--- v3/sli/src/util/errcode.c
+++ v3/sli/src/util/errcode.c
@@ -1,6 +1,6 @@
 #include "errcode.h"
 
-const char *sl_errcode_lookup[13] = {
+const char *sl_errcode_lookup[14] = {
     "SL_ERR_OK",
     "SL_ERR_NULL_PTR",
     "SL_ERR_OUT_OF_MEMORY",
@@ -13,5 +13,6 @@ const char *sl_errcode_lookup[13] = {
     "SL_ERR_MISSING_OPERAND",
     "SL_ERR_TYPE_MISMATCH",
     "SL_ERR_INVALID_COMMAND",
-    "SL_ERR_INTERPRETER_NOT_INITIALIZED"
+    "SL_ERR_INTERPRETER_NOT_INITIALIZED",
+    "SL_ERR_IDX_OUT_OF_BOUND"
 };
--- v3/sli/src/util/errcode.h
+++ v3/sli/src/util/errcode.h
@@ -14,9 +14,10 @@ typedef enum SLErrCode {
     SL_ERR_MISSING_OPERAND,
     SL_ERR_TYPE_MISMATCH,
     SL_ERR_INVALID_COMMAND,
-    SL_ERR_INTERPRETER_NOT_INITIALIZED
+    SL_ERR_INTERPRETER_NOT_INITIALIZED,
+    SL_ERR_IDX_OUT_OF_BOUND
 } SLErrCode;
 
-extern const char *sl_errcode_lookup[13];
+extern const char *sl_errcode_lookup[14];
 
 #endif
