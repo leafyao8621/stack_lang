75e78726088f12b58a360a57872548eec0d2471f
Yi Yao <leaf_yao8621@hotmail.com>
2023-06-27 11:58:42 -0700

    Updates

--- v3/sli/src/interpreter/core/core_run_handlers/operator_handlers/handle_operator_add_assign.c
+++ v3/sli/src/interpreter/core/core_run_handlers/operator_handlers/handle_operator_add_assign.c
@@ -39,6 +39,16 @@ SLErrCode runtime_handle_operator_add_assign(SLInterpreter *interpreter) {
         case SL_VARIABLE_LOCATION_GLOBAL:
             op_a_int_var = (int64_t*)(interpreter->global.data + offset);
             break;
+        case SL_VARIABLE_LOCATION_DIRECT:
+            op_a_int_var =
+                (int64_t*)
+                    interpreter
+                        ->operation_stack
+                        .data[interpreter->operation_stack.size]
+                        .data
+                        .int_var
+                        .direct;
+            break;
         default:
             break;
         }
@@ -74,6 +84,16 @@ SLErrCode runtime_handle_operator_add_assign(SLInterpreter *interpreter) {
             case SL_VARIABLE_LOCATION_GLOBAL:
                 op_b_int = *(int64_t*)(interpreter->global.data + offset);
                 break;
+            case SL_VARIABLE_LOCATION_DIRECT:
+                op_b_int =
+                    *(int64_t*)
+                        interpreter
+                            ->operation_stack
+                            .data[interpreter->operation_stack.size + 1]
+                            .data
+                            .int_var
+                            .direct;
+                break;
             default:
                 break;
             }
@@ -106,6 +126,16 @@ SLErrCode runtime_handle_operator_add_assign(SLInterpreter *interpreter) {
             case SL_VARIABLE_LOCATION_GLOBAL:
                 op_b_float = *(double*)(interpreter->global.data + offset);
                 break;
+            case SL_VARIABLE_LOCATION_DIRECT:
+                op_b_float =
+                    *(double*)
+                        interpreter
+                            ->operation_stack
+                            .data[interpreter->operation_stack.size + 1]
+                            .data
+                            .float_var
+                            .direct;
+                break;
             default:
                 break;
             }
@@ -132,6 +162,16 @@ SLErrCode runtime_handle_operator_add_assign(SLInterpreter *interpreter) {
                 .location) {
         case SL_VARIABLE_LOCATION_GLOBAL:
             op_a_float_var = (double*)(interpreter->global.data + offset);
+            break;
+        case SL_VARIABLE_LOCATION_DIRECT:
+            op_a_float_var =
+                (double*)
+                    interpreter
+                        ->operation_stack
+                        .data[interpreter->operation_stack.size]
+                        .data
+                        .float_var
+                        .direct;
             break;
         default:
             break;
@@ -168,6 +208,16 @@ SLErrCode runtime_handle_operator_add_assign(SLInterpreter *interpreter) {
             case SL_VARIABLE_LOCATION_GLOBAL:
                 op_b_int = *(int64_t*)(interpreter->global.data + offset);
                 break;
+            case SL_VARIABLE_LOCATION_DIRECT:
+                op_b_int =
+                    *(int64_t*)
+                        interpreter
+                            ->operation_stack
+                            .data[interpreter->operation_stack.size + 1]
+                            .data
+                            .int_var
+                            .direct;
+                break;
             default:
                 break;
             }
@@ -199,6 +249,16 @@ SLErrCode runtime_handle_operator_add_assign(SLInterpreter *interpreter) {
                     .location) {
             case SL_VARIABLE_LOCATION_GLOBAL:
                 op_b_float = *(double*)(interpreter->global.data + offset);
+                break;
+            case SL_VARIABLE_LOCATION_DIRECT:
+                op_b_float =
+                    *(double*)
+                        interpreter
+                            ->operation_stack
+                            .data[interpreter->operation_stack.size + 1]
+                            .data
+                            .float_var
+                            .direct;
                 break;
             default:
                 break;
